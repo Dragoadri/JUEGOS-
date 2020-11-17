cb79ab8b112de563e30e048239fa6b642e4041d1
22004996 <22004996@C306-24.alumnos.uem.es>
2020-11-17 17:10:11 +0100

    Avance en Ajedrez

--- src/Ajedrez/Pieza.java
+++ src/Ajedrez/Pieza.java
@@ -3,13 +3,30 @@ package Ajedrez;
 public class Pieza {
 	private String color;
 	private String nombrePieza;
+	private String posicion;
 	private boolean viva;
 
-	public Pieza(String color, String nombrePieza, boolean viva) {
+
+	
+
+	public Pieza(String color, String nombrePieza, String posicion, boolean viva) {
+		super();
 		this.color = color;
 		this.nombrePieza = nombrePieza;
+		this.posicion = posicion;
 		this.viva = viva;
 	}
+	
+
+	public String getPosicion() {
+		return posicion;
+	}
+
+
+	public void setPosicion(String posicion) {
+		this.posicion = posicion;
+	}
+
 
 	public String getNombrePieza() {
 		return nombrePieza;
